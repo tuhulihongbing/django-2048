913c607334b7ef4d97e5ead0f10c357a2d361d82
tuhulihongbing <lihongbing@tuhu.cn>
2016-11-12 11:38:53 +0800

    add  trains model

--- trains/models.py
+++ trains/models.py
@@ -5,9 +5,20 @@ from django.db import models
 
 
 # Create your models here.
+class RawTrain(models.Model):
+    name = models.CharField("车次", max_length=50)
+    std_no = models.ForeignKey('Train', verbose_name='标准车次')
+
+    def __unicode__(self):
+        return self.name
+
 class Train(models.Model):
+    types = ((0, "已停用"),
+             (1, "使用中"),
+             (2, "未知"))
     train_no = models.CharField("车次", max_length=50)
     stations = models.ManyToManyField('Station', through='Membership')
+    status =  models.IntegerField(choices=types, verbose_name='状态', default=1)
 
     def __unicode__(self):
         return self.train_no
@@ -19,9 +30,9 @@ class Station(models.Model):
         return self.name
 
 class Membership(models.Model):
-    types = {0: "始发站",
-             1: "中途站",
-             2: "终点站"}
+    types = ((0, "始发站"),
+             (1, "中途站"),
+             (2, "终点站"))
     train = models.ForeignKey(Train , verbose_name='车次')
     station = models.ForeignKey(Station, verbose_name="站名")
     arrive_time = models.TimeField("到站时间")
