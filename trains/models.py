a80e0791900ee4003997288ea428bc04d446b11e
tuhulihongbing <lihongbing@tuhu.cn>
2016-11-12 11:32:16 +0800

    add  trains model

--- trains/models.py
+++ trains/models.py
@@ -6,24 +6,28 @@ from django.db import models
 
 # Create your models here.
 class Train(models.Model):
-    train_no = models.CharField("输入车次", max_length=30)
-    std_train_no = models.CharField("标准车次", max_length=50)
-    start_station = models.CharField("始发站",max_length=30)
-    start_time = models.TimeField("始发时间")
-    end_station = models.CharField("终点站", max_length=30)
-    end_time = models.TimeField("到达时间")
+    train_no = models.CharField("车次", max_length=50)
+    stations = models.ManyToManyField('Station', through='Membership')
 
     def __unicode__(self):
         return self.train_no
 
 class Station(models.Model):
-    types = {0: "始发站",
-             1: "中途站",
-             2: "终点站"}
     name = models.CharField("站名", max_length=30)
-    arrive_time = models.TimeField("到站时间")
-    out_time = models.TimeField("出站时间")
-    station_type  = models.IntegerField(choices=types,verbose_name='车站类型',default=1)
 
     def __unicode__(self):
         return self.name
+
+class Membership(models.Model):
+    types = {0: "始发站",
+             1: "中途站",
+             2: "终点站"}
+    train = models.ForeignKey(Train , verbose_name='车次')
+    station = models.ForeignKey(Station, verbose_name="站名")
+    arrive_time = models.TimeField("到站时间")
+    out_time = models.TimeField("发车时间")
+    sequences = models.IntegerField("发车顺序")
+    station_type = models.IntegerField(choices=types, verbose_name='车站类型', default=1)
+
+    def __unicode__(self):
+        return self.station_type
